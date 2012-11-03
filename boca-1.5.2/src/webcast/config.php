f099cf6214a24104e4b3d66bbda110cdb1703ac2
cassiopc <cassiopc@gmail.com>
2012-11-03 12:03:02 +0100

    inclusion of automatic configuration for webcast according to the site info; permission of accessing webcast to admin or score typed users

--- boca-1.5.2/src/webcast/config.php
+++ boca-1.5.2/src/webcast/config.php
@@ -1,8 +1,25 @@
 <?php
+$loc = $locr = "..";
+require_once("$locr/globals.php");
+require_once("$locr/db.php");
+if(!ValidSession()) {
+	InvalidSession("webcast/index.php");
+        ForceLoad("$loc/index.php");
+}
+if($_SESSION["usertable"]["usertype"] != "admin" && $_SESSION["usertable"]["usertype"] != "score") {
+	IntrusionNotify("webcast/index.php");
+	ForceLoad("$loc/index.php");
+}
 
-$contest = 1;
-$site = 1;
+$contest = $_SESSION["usertable"]["contestnumber"];
+$site = $_SESSION["usertable"]["usersitenumber"];
 
-$freezeTime = 240;
+if(($ct =  DBSiteInfo($contest, $site)) == null)
+	ForceLoad("../index.php");
+
+if(isset($_GET['full']) && $_GET['full'] > 0 && $_SESSION["usertable"]["usertype"] == "admin")
+	$freezeTime = $ct['siteduration'];
+else
+	$freezeTime = $ct['sitelastmilescore'];
 
 ?>
