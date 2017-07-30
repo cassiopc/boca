62f63132adee7d06deebc0162b0807885e8884ff
Cassio de Campos <cassiopc@gmail.com>
2017-07-30 17:31:17 +0100

    bug fixes

--- src/site/getsite.php
+++ src/site/getsite.php
@@ -30,27 +30,30 @@ if($ct["contestlocalsite"]==$ct["contestmainsite"]) {
 	LOGLevel("Connection received from site=$fromsite PHPID=".$_COOKIE['PHPSESSID'].",extra=".$_SESSION['usertable']['usersessionextra'].
 			 ",session=".session_id().",name=".$getx['name'].", check=".$getx['check'],2);
 	if($fromsite != '' && is_numeric($fromsite) && $fromsite > 0) {
-	} else {
-		echo "<!-- <ERROR9> ".session_id() . " " . session_id() . " -->\n";
-		exit;
-	}
-	if(isset($_POST)) {
-	  if(isset($_POST['xml'])) {
-//		$fp=fopen('/tmp/aaa',"w"); fwrite($fp,$_POST['xml']); fclose($fp);
-		$s = decryptData(rawurldecode($_POST['xml']),myhash($_SESSION["usertable"]["userpassword"]));
-//		$fp=fopen('/tmp/aaa1',"w"); fwrite($fp,$s); fclose($fp);
-		if(strtoupper(substr($s,0,5)) != "<XML>") {
-		  echo "<!-- <ERROR8> ".session_id() . " " . session_id() . " -->\n";
-		} else {
-		  if(importFromXML($s,$_SESSION["usertable"]["contestnumber"],$fromsite,true))
-		    echo "<!-- <OK> -->";
-		  else
-		    echo "<!-- <NOTOK> -->";
-		}
+	  if(isset($_POST)) {
+	    if(isset($_POST['xml'])) {
+	      //		$fp=fopen('/tmp/aaa',"w"); fwrite($fp,$_POST['xml']); fclose($fp);
+	      $s = decryptData(rawurldecode($_POST['xml']),myhash($_SESSION["usertable"]["userpassword"]));
+	      //		$fp=fopen('/tmp/aaa1',"w"); fwrite($fp,$s); fclose($fp);
+	      if(strtoupper(substr($s,0,5)) != "<XML>") {
+		echo "<!-- <ERROR8> ".session_id() . " " . session_id() . " -->\n";
+	      } else {
+		if(importFromXML($s,$_SESSION["usertable"]["contestnumber"],$fromsite,true))
+		  echo "<!-- <OK> -->";
+		else
+		  echo "<!-- <NOTOK> -->";
+	      }
+	    }
+	    if(isset($_POST['updatetime']) && is_numeric($_POST['updatetime'])) {
+	      $xml = generateSiteXML($_SESSION["usertable"]["contestnumber"],$fromsite,$_POST['updatetime']);
+	      echo encryptData($xml,myhash($_SESSION["usertable"]["userpassword"]));
+	    }
+	  } else { 
+	    echo "<!-- <ERROR3> ".session_id() . " " . session_id() . " -->\n";
 	  }
-	  if(isset($_POST['updatetime']) && is_numeric($_POST['updatetime'])) {
-	    $xml = generateSiteXML($_SESSION["usertable"]["contestnumber"],$fromsite,$_POST['updatetime']);
-	echo encryptData($xml,myhash($_SESSION["usertable"]["userpassword"]));
-} else 
-	echo "<!-- <ERROR3> ".session_id() . " " . session_id() . " -->\n";
+	} else {
+	  echo "<!-- <ERROR9> ".session_id() . " " . session_id() . " -->\n";
+	  exit;
+	}
+}
 ?>
