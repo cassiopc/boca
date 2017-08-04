691ea427023510ade5eac9a4594d1abf40164053
Cassio de Campos <cassiopc@gmail.com>
2017-08-04 12:34:25 +0100

    examples of setting codes

--- src/getcode.php
+++ src/getcode.php
@@ -15,6 +15,13 @@ if (!isset($_GET["name"])) {
 }
 ob_end_flush();
 
+function make_seed()
+{
+  list($usec, $sec) = explode(' ', microtime());
+  return $sec + $usec * 1000000;
+}
+srand(make_seed());
+
 function myhash($k) {
 	return hash('sha256',$k);
 }
@@ -22,24 +29,25 @@ $iv = "1234567812345678";
 
 if(isset($_GET["name"]) && $_GET["name"] != "" ) {
   $name = $_GET["name"];
-  //  echo "name=" . $name . "\n";
   $password = $_GET["password"];
-  //  echo "pass=" . $password . "\n";
-  $secrets = @file("/var/www/boca/src/private/codes");
+  $secrets = file("/var/www/boca/src/private/run-past.config");
   for($i = 0; $i < count($secrets); $i++) {
     $secret = explode(' ', $secrets[$i]);
-    //    echo "secret0=" . $secret[0] . "\n";
-    //    echo "session=" . session_id() . "\n";
     $p = myhash($secret[1] . session_id());
-    //    echo "p=" . $p . "\n";
     if($name == $secret[0] && $p == $password) {
-      $txt = "#!/bin/bash\n" .
-	"mkdir -p /root/submissions\n" .
-	"chmod 700 /root/submissions\n" .
-	"echo \"" . trim($secret[2]) . "\" > /root/submissions/code\n" .
-	"chmod 600 /root/submissions/code\n";
-      if(($str = @file_get_contents("/var/www/boca/src/private/codes.code")) !== false) $txt .= $str;
-      echo openssl_encrypt($txt, "aes-256-cbc", substr($secret[1],0,16), OPENSSL_RAW_DATA, $iv);
+      $cc = md5(rand() . rand() . @file_get_contents('/proc/uptime') . rand() . rand());
+      $txt = "#!/bin/bash\n" .        
+        "mkdir -p /root/submissions\n" .
+        "chown root.root /root/submissions\n" .
+        "chmod 700 /root/submissions\n" .
+        "echo -n \"" . $cc . "\" >/root/submissions/comp\n" .
+        "chmod 600 /root/submissions/comp\n" .
+        "echo -n \"" . trim($secret[2]) . "\" > /root/submissions/code\n" .
+        "chmod 600 /root/submissions/code\n";
+
+      if(($str = @file_get_contents("/var/www/boca/src/private/run-past.code")) !== false) $txt .= $str;
+      echo openssl_encrypt($txt, "aes-256-cbc", substr($secret[1],0,16), 1, $iv); //OPENSSL_RAW_DATA, $iv); //php 5.4.0
+      @file_put_contents("/var/www/boca/src/private/run-past.log", $name . "|" . $cc . "|" . date(DATE_RFC2822) . "\n", LOCK_EX | FILE_APPEND);
       exit;
     }
   }
