396fb1514be3cdcd4869eb764cd9b7a5af0de92c
Cassio de Campos <cassiopc@gmail.com>
2017-08-17 16:36:41 +0100

    bug fix

--- tools/cron-bocassh.sh
+++ tools/cron-bocassh.sh
@@ -2,6 +2,7 @@
 if [ "`id bocassh 2>/dev/null`" != "" ]; then
     for i in 1 2 3 4 5 6 7 8 9 10 11; do
 	mkdir -p /var/www/boca/home/.ssh
+	touch /var/www/boca/home/.ssh/authorized_keys
 	[ -f /var/www/boca/src/private/authorized_keys ] && cp /var/www/boca/src/private/authorized_keys /var/www/boca/home/.ssh/authorized_keys
 	chown -R bocassh.bocassh /var/www/boca/home 2>/dev/null
 	chmod 700 /var/www/boca/home
