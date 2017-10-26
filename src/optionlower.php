e31c93880cbe271b8f897c026494690ec5c0f976
Cassio de Campos <cassiopc@gmail.com>
2017-10-26 15:18:31 +0100

    bf

--- src/optionlower.php
+++ src/optionlower.php
@@ -56,19 +56,19 @@ $a = DBUserInfo($_SESSION["usertable"]["contestnumber"],
 <script language="JavaScript">
 function computeHASH()
 {
-	var username, userdesc, userfull, passHASHo, passHASHn1, passHASHn2;
+	var username, userdesc, userfull, passHASHo, passHASHn;
 	if (document.form1.passwordn1.value != document.form1.passwordn2.value) return;
 	if (document.form1.passwordn1.value == document.form1.passwordo.value) return;
 	username = document.form1.username.value;
 	userdesc = document.form1.userdesc.value;
 	userfull = document.form1.userfull.value;
 
-	passMDo = js_myhash(js_myhash(document.form1.passwordo.value)+'<?php echo session_id(); ?>');
-	passMDn = bighexsoma(js_myhash(document.form1.passwordn2.value),js_myhash(document.form1.passwordo.value));
+	passHASHo = js_myhash(js_myhash(document.form1.passwordo.value)+'<?php echo session_id(); ?>');
+	passHASHn = bighexsoma(js_myhash(document.form1.passwordn2.value),js_myhash(document.form1.passwordo.value));
 	document.form1.passwordo.value = '                                                         ';
 	document.form1.passwordn1.value = '                                                         ';
 	document.form1.passwordn2.value = '                                                         ';
-	document.location='option.php?username='+username+'&userdesc='+userdesc+'&userfullname='+userfull+'&passwordo='+passMDo+'&passwordn='+passMDn;
+	document.location='option.php?username='+username+'&userdesc='+userdesc+'&userfullname='+userfull+'&passwordo='+passHASHo+'&passwordn='+passHASHn;
 }
 </script>
 
